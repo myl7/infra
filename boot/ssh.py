0fc36c93d96061158402f9cf044e2472dbe1c4f4
myl7 <myl@myl.moe>
2024-07-18 15:08:39 +0800

    Fix ssh bootloader

--- boot/ssh.py
+++ boot/ssh.py
@@ -5,7 +5,7 @@ import os
 
 parser = argparse.ArgumentParser(prog='boot/ssh')
 parser.add_argument('-p', '--port', type=int, default=22)
-parser.add_argument('-t', '--tmpdir', default='/tmp')
+parser.add_argument('-t', '--tmp-dir', default='/tmp')
 parser.add_argument('-s', '--sudo', action='store_true')
 parser.add_argument('-r', '--recursive', action='store_true')
 parser.add_argument('-e', '--exec', action='store_true')
@@ -23,16 +23,22 @@ if ':' in src:
 if ':' in dst:
     ssh_uri, dst = dst.split(':', 1)
 
-
 ssh_cmd = ['ssh', '-p', str(args.port), ssh_uri]
 rsync_cmd = ['rsync', '-re' if args.recursive else '-e', f'ssh -p {args.port}']
+
+
+def basename(p: str) -> str:
+    fname = os.path.basename(p)
+    if fname == '':
+        fname = os.path.basename(p[:-1]) + '/'
+    return fname
 
 
 if is_pull or not args.sudo:
     subprocess.check_call(rsync_cmd + [args.src, args.dst])
 else:
-    tmp_fname = os.path.basename(src)
-    tmp_path = os.path.join(args.tmpdir, tmp_fname)
+    tmp_fname = basename(src)
+    tmp_path = os.path.join(args.tmp_dir, tmp_fname)
     subprocess.check_call(rsync_cmd + [src, ssh_uri + ':' + tmp_path])
     subprocess.check_call(ssh_cmd + ['sudo', 'rsync', '-rI' if args.recursive else '-I', tmp_path, dst])
 if args.exec:
