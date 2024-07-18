2fd7c2971095788fa49d65c08adaaaaadd3df980
myl7 <myl@myl.moe>
2024-07-18 14:16:07 +0800

    Add creating user

--- sbin/prelude.sh
+++ sbin/prelude.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-sudo apt-get install -y \
-  curl wget git neovim zsh rsync tmux \
+apt-get install -y \
+  sudo curl wget git neovim zsh rsync tmux \
   net-tools lsof dnsutils nethogs htop iotop nmap tcpdump \
   python3 python3-venv pipx build-essential
--- sbin/user.sh
+++ sbin/user.sh
@@ -3,5 +3,6 @@ set -euo pipefail
 
 INFRA_USER=myl
 
+useradd -m "$INFRA_USER"
 echo "$INFRA_USER ALL=(ALL:ALL) NOPASSWD: ALL" > "/etc/sudoers.d/$INFRA_USER"
 usermod -aG sudo "$INFRA_USER"
